cf327eb3075ae0293fdbb0dad66562ad488bab73
Jason Sejik Lee <gggabikr@gmail.com>
2022-08-22 21:49:28 +0900

    Update Item.java

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.domain.item;
 
+import jpabook.jpashop.domain.Category;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -23,5 +24,6 @@ public abstract class Item {
 
     private int stockQuantity;
 
-//    private List<Category> categories = new ArrayList<>();
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categories = new ArrayList<>();
 }
